289fddc3d402759d653d9f6e960ff5d1cb3d413c
isabelleromhagenBiswise <isabelle.romhagen@biswise.se>
2020-12-07 20:14:36 +0100

    created routes for api

--- routes/users.js
+++ routes/users.js
@@ -7,17 +7,14 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 const normalize = require("normalize-url");
-
 const User = require("../models/User");
 
 // @route    GET /users/me
 // @desc     Get current users profile
 // @access   Private
-
 router.get("/me", auth, async (req, res) => {
   try {
       if (mongoose.isValidObjectId(req.user.id)) {
-        const objectId = mongoose.Types.ObjectId(req.user.id);
         const currentUser = await User.findById(req.user.id).populate("user", ["username"]);
 
         if (!currentUser) {
@@ -109,26 +106,68 @@ router.get("/", async (req, res) => {
   }
 });
 
-// @route    GET api/profile/user/:user_id
+// @route    GET /users/:user_id
 // @desc     Get profile by user ID
 // @access   Public
-// router.get("/user/:user_id", 
-//   checkObjectId("user_id"),
-//   async ({ params: { user_id } }, res) => {
-//     try {
-//       const user = await User.findOne({ 
-//         user: user_id
-//       }).populate("user", ["username"]);
+router.get("/:user_id", async ({ params: { user_id } }, res) => {
+    try {
+        if(mongoose.isValidObjectId(user_id)) {
+            const user = await User.findById(user_id).populate("user", ["username"]);
 
-//       if (!profile) return res.status(400).json({ msg: "Profile not found" });
+            if (!user) return res.status(400).json({ msg: "User not found" });
+            return res.json(user);
+        }
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ msg: "Server error" });
+    }
+  }
+);
 
-//       return res.json(user);
-//     } catch (err) {
-//       console.error(err.message);
-//       return res.status(500).json({ msg: "Server error" });
-//     }
-//   }
-// );
+// @route    PUT /users
+// @desc     Update user profile
+// @access   Private
+router.put("/", auth, async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        const { username, bio } = req.body;
+
+        try {
+            if(mongoose.isValidObjectId(req.user.id)) {
+                const profile = await User.findByIdAndUpdate(
+                {_id: req.user.id},
+                {$set: {username: username, bio: bio}},
+                {new: true, upsert: true, setDefaultsOnInsert: true}
+        );
+        return res.json(profile);
+            }
+        } catch (error) {
+            console.error(error.message);
+            return res.status(500).send("Server error")
+        }
+    }
+);
+
+
+
+// @route    DELETE /users/:user_id
+// @desc     Delete account
+// @access   Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    await User.findOneAndRemove(req.user.id);
+
+    res.json({msg: "User deleted"});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({msg: "Server error"});
+  }
+});
+
+
+// --------------------------------
 
 // router.get("/", auth, async (req, res) => {
 //   try {
