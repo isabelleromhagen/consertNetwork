76eeb618861ca7844d810885cbbb9b40d7d74365
isabelleromhagenBiswise <isabelle.romhagen@biswise.se>
2021-01-28 12:37:30 +0100

    heroku

--- server.js
+++ server.js
@@ -71,14 +71,19 @@ app.use("/", imageRouter(upload));
 // end of GridFS
 
 //Heroku
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production') {
+    // app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
+// }
 
+// Heroku try again...
+app.use(express.static(path.join(__dirname, "/client/build/")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 8080;
 
